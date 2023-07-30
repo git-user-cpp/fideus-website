bc903a754c595f8dd533604776e2e2cc8a61c366
git-user-cpp <akushyk799@gmail.com>
2023-07-30 15:22:01 +0300

    completed basic structure

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
+import Footer from './Footer'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+/*
+Copyright 2023 Andrew Kushyk
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+};
